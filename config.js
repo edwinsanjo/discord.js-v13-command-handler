16e21f40c544ea542bce7aa4b8f123f957eced59
Edwin sanjo soji <72296542+edwinsanjo@users.noreply.github.com>
2021-12-31 09:11:32 +0530

    v3 bugs solved

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     prefix: "-",
-    mongo: process.env.MONGO || "mongodb+srv://EDWIN:EDWINSANJO@cluster0.rwssd.mongodb.net/cmd-handler?retryWrites=true&w=majority",
+    mongo: process.env.MONGO || "",
     testGuildIDS: "854588598526148628",
     slashGlobal: false,
     ownerID: "763978445847658527",
-    token: process.env.TOKEN || "ODg4MDY4NDgyOTk3MDM5MTU0.YUNUOQ.ZIxGPiA1ncL6QMRvXZVpe6RT5Ms"
+    token: process.env.TOKEN || ""
 }
